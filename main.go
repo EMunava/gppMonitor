c95d01b71dc238570c74d0301ca38b844ac2a371
Jurgen_Schatz <jurgen.schatz@gmail.com>
2018-01-15 13:47:52 +0200

    Fixed main fucntion

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
-package GPPMonitor
+package main
 
-import "log"
+import (
+	"log"
+	_ "github.com/CardFrontendDevopsTeam/GPPMonitor/selenium"
+	"time"
+)
 
 func main() {
 	log.Println("GPP Monitor")
-
+	for true {
+		time.Sleep(10 * time.Minute)
+	}
 }
