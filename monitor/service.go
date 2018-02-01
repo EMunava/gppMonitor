e2064f0109f734e146981ea1758290f97e737416
Marc Arndt <marc@marcarndt.com>
2018-02-01 12:56:12 +0200

    adding eod log service

--- monitor/service.go
+++ monitor/service.go
@@ -5,6 +5,8 @@ import (
 	"github.com/CardFrontendDevopsTeam/GPPMonitor/daterollover"
 	"github.com/zamedic/go2hal/alert"
 	"github.com/CardFrontendDevopsTeam/GPPMonitor/waitSchduleBatch"
+	"github.com/CardFrontendDevopsTeam/GPPMonitor/sftp"
+	"github.com/CardFrontendDevopsTeam/GPPMonitor/eodLog"
 )
 
 type Service interface {
@@ -14,19 +16,24 @@ type service struct {
 	alert alert.Service
 }
 
-func NewService(alert alert.Service)Service{
-	return &service{alert:alert}
+func NewService(alert alert.Service) Service {
+	s := &service{alert: alert}
+	s.schedule()
+	return s
 }
 
-
 func (s *service) schedule() {
+	sftpService := sftp.NewService()
+
 	sel := gocron.NewScheduler()
 	dateRollover := func() { daterollover.NewService(s.alert).ConfirmDateRollOver() }
 	scheduleBatch := func() { waitSchduleBatch.NewService(s.alert).ConfirmWaitSchedSubBatch() }
+	eofFileCheck := func() { eodLog.NewService(sftpService, s.alert).RetrieveEDOLog() }
+
 	sel.Every(1).Day().At("23:30").Do(dateRollover)
 	sel.Every(1).Day().At("00:30").Do(dateRollover)
+	sel.Every(1).Day().At("01:10").Do(eofFileCheck)
 	sel.Every(1).Day().At("01:30").Do(dateRollover)
-
 	sel.Every(1).Day().At("00:35").Do(scheduleBatch)
 
 	gocron.NextRun()
