c7a3552c8c46c7f5ad02bdd69ed6605280d3eded
Jurgen_Schatz <jurgen.schatz@gmail.com>
2018-03-08 11:05:54 +0200

    EDO log timestamp parse & Transaction notification rewording

--- eodLog/service_test.go
+++ eodLog/service_test.go
@@ -3,9 +3,9 @@ package eodLog
 import "testing"
 
 func TestDateConvert(t *testing.T) {
-	dateResult := dateConvert("2018-03-05 14:08:49.723242043 +0200 SAST m=+0.000201828")
+	dateResult, timeResult := dateTimeConvert("2018-03-05 14:08:49.723242043 +0200 SAST m=+0.000201828", "11:49:30")
 
-	if dateResult != "01/01/0001" {
+	if dateResult != "01/01/0001" && timeResult == "11:49AM" {
 		t.Errorf("Date was not propperly formatted, got: %s, want: %s.", dateResult, "05/03/2018")
 	}
 }
